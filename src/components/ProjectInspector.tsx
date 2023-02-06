f56c1b13feceb26759ec182c32ea3b14436f4de5
max-hans <m.hans@me.com>
2023-02-06 20:21:10 +0100

    add in project inspector

--- src/components/ProjectInspector.tsx
+++ src/components/ProjectInspector.tsx
@@ -1,29 +1,48 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { Project } from "../types/data";
 
 export interface ProjectInspectorProps {
-  open: boolean;
   projects: Project[];
-  selected: number;
+  selected: number | null;
   onDeselect: () => void;
 }
 
 const ProjectInspector: FC<ProjectInspectorProps> = ({
-  open,
   projects,
   selected,
   onDeselect,
 }) => {
-  const project = projects[selected];
+  const [project, setProject] = useState<Project>();
+
+  const [open, setOpen] = useState(false);
+
+  /* const project: Project | undefined =
+    selected !== null ? projects[selected] : undefined; */
+
+  /* const tempProjectRef = useRef<Project>(); */
+
+  /* if (selected !== null) {
+    tempProjectRef.current = project;
+    console.log(tempProjectRef.current);
+  } */
+
+  useEffect(() => {
+    if (selected !== null) {
+      setProject(projects[selected]);
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }, [selected]);
 
   return (
     <div
       className={`fixed top-16 bg-white p-4 flex flex-col space-y-4 rounded-md shadow-md w-72 z-50
-	transition-all right-0 ${open ? "" : "translate-x-72"}`}
+	transition-all left-0 ${open ? "" : "-translate-x-72"}`}
     >
       <div
         className="flex flex-col space-y-4 overflow-hidden"
-        style={{ display: open ? "visible" : "hidden" }}
+        /* style={{ display: open ? "visible" : "hidden" }} */
       >
         {project && (
           <>
--- src/routes/map/index.tsx
+++ src/routes/map/index.tsx
@@ -11,6 +11,7 @@ import {
   projectsAsset,
   scenariosAsset,
 } from "../../data/assets";
+import ProjectInspector from "../../components/ProjectInspector";
 
 const Map = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -18,7 +19,6 @@ const Map = () => {
 
   const projects = projectsAsset.read();
   const positions = positionsAsset.read();
-  /* const scenarios = scenariosAsset.read(); */
 
   const [mode, setMode] = useState<DisplayMode>("HISTORY");
   const [selected, setSelected] = useState<number | null>(null);
@@ -111,51 +111,58 @@ const Map = () => {
   }, [time]);
 
   return (
-    <div className=" w-full h-full flex flex-col justify-center items-center p-8 relative">
-      <div className="bg-blue-500 relative aspect-video rounded-md overflow-hidden shadow-lg h-full">
-        <div className="absolute top-0 left-0 w-full h-full">
-          <canvas
-            className="w-full aspect-video"
-            ref={canvasRef}
-            width={MAP_SIZE_X}
-            height={MAP_SIZE_Y}
-          />
-          {/* <MapRenderer {history} {time} activeScenario={scenarioIdx} {mode} /> */}
-        </div>
-        <div className="absolute top-0 left-0 z-10 w-full h-full">
-          <svg
-            viewBox="0 0 1920 1080"
-            className="absolute top-0 left-0 w-full h-full"
-          >
-            <>
-              {projects.map((project, idx) => {
-                return (
-                  <circle
-                    cx={`${project.position.x * 100}%`}
-                    cy={`${project.position.y * 100}%`}
-                    r="8"
-                    fill="black"
-                    onClick={() => {
-                      setSelected(idx);
-                    }}
-                  />
-                );
-              })}
+    <>
+      <div className=" w-full h-full flex flex-col justify-center items-center p-8 relative">
+        <div className="bg-blue-500 relative aspect-video rounded-md overflow-hidden shadow-lg h-full">
+          <div className="absolute top-0 left-0 w-full h-full">
+            <canvas
+              className="w-full aspect-video"
+              ref={canvasRef}
+              width={MAP_SIZE_X}
+              height={MAP_SIZE_Y}
+            />
+            {/* <MapRenderer {history} {time} activeScenario={scenarioIdx} {mode} /> */}
+          </div>
+          <div className="absolute top-0 left-0 z-10 w-full h-full">
+            <svg
+              viewBox="0 0 1920 1080"
+              className="absolute top-0 left-0 w-full h-full"
+            >
+              <>
+                {projects.map((project, idx) => {
+                  return (
+                    <circle
+                      cx={`${project.position.x * 100}%`}
+                      cy={`${project.position.y * 100}%`}
+                      r="8"
+                      fill="black"
+                      onClick={() => {
+                        setSelected(idx);
+                      }}
+                    />
+                  );
+                })}
 
-              {positions.map((position) => {
-                <circle
-                  className="pointer-cursor"
-                  cx={`${position.x * 100}%`}
-                  cy={`${position.y * 100}%`}
-                  r="2"
-                  fill="black"
-                />;
-              })}
-            </>
-          </svg>
+                {positions.map((position) => {
+                  <circle
+                    className="pointer-cursor"
+                    cx={`${position.x * 100}%`}
+                    cy={`${position.y * 100}%`}
+                    r="2"
+                    fill="black"
+                  />;
+                })}
+              </>
+            </svg>
+          </div>
         </div>
       </div>
-    </div>
+      <ProjectInspector
+        projects={projects}
+        selected={selected}
+        onDeselect={() => setSelected(null)}
+      />
+    </>
   );
 };
 export default Map;
