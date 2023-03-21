ff52d75283e80579a956bbcf32edb7bb7b573a0e
max-hans <m.hans@me.com>
2023-03-21 21:22:00 +0100

    add in movement keys

--- src/routes/map/Scene.tsx
+++ src/routes/map/Scene.tsx
@@ -1,11 +1,10 @@
-import { useFrame, useLoader, useThree } from "@react-three/fiber";
-import { Suspense, useEffect, useRef } from "react";
-import { animated } from "@react-spring/three";
+import { useLoader, useThree } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
 
 import { history } from "../../data/CONSTANTS";
 import VideoMaterial from "./comps/VideoMaterial";
 import useMainStore from "../../stores/main";
-import { remap } from "../../func/data";
+import { constrain, remap } from "../../func/data";
 
 import useUiStore from "../../stores/ui";
 
@@ -13,19 +12,62 @@ import futures from "./res/futures";
 
 import { CameraControls } from "@react-three/drei";
 import { MAX_ZOOM, MIN_ZOOM } from "../../CONSTANTS";
-import { useEffectOnce } from "react-use";
-import { TextureLoader } from "three";
+import { useEffectOnce, useKeyPressEvent } from "react-use";
+import { MathUtils, Mesh, TextureLoader } from "three";
+import { Vec2D } from "@/types/data";
+import { update } from "@react-spring/web";
 
 const calcWfromH = (h: number): number => {
   const w = h / (1080 / 1920);
-  console.log(w);
-
   return w;
 };
+
+const MOVE_DELTA = 100;
 
 const Scene = () => {
   const { viewport, camera } = useThree();
   const scenarios = useLoader(TextureLoader, futures);
+  const uiConfig = useUiStore();
+  const meshRef = useRef<Mesh>(null);
+
+  const viewportSizeRef = useRef({ width: -1, height: -1 });
+
+  const [pos, setPos] = useState<Vec2D>({ x: 0, y: 0 });
+
+  const [planeSize] = useState(() => {
+    const h = viewport.height;
+    const w = calcWfromH(h);
+    return [w, h];
+  });
+
+  const updateCamera = (pos: Vec2D) => {
+    if (!cameraControlsRef.current) return;
+    cameraControlsRef.current.moveTo(pos.x, pos.y, 0, true);
+  };
+
+  const moveCamera = (dir: Partial<{ x: number; y: number }>) => {
+    setPos(() => {
+      const tempPos = { ...pos };
+      const { width, height } = viewportSizeRef.current;
+
+      const maxX = (planeSize[0] - width / uiConfig.targetZoomFactor) / 2;
+      const maxY = (planeSize[1] - height / uiConfig.targetZoomFactor) / 2;
+
+      if (dir.x) {
+        tempPos.x = tempPos.x + dir.x;
+      }
+      if (dir.y) {
+        tempPos.y = tempPos.y + dir.y;
+      }
+      const newPos = {
+        x: constrain(tempPos.x, -maxX, maxX),
+        y: constrain(tempPos.y, -maxY, maxY),
+      };
+
+      console.log(maxX, newPos.x);
+      return newPos;
+    });
+  };
 
   const [time, scenarioIndex, mode] = useMainStore((state) => [
     state.time,
@@ -38,16 +80,26 @@ const Scene = () => {
   useEffectOnce(() => {
     if (!cameraControlsRef.current) return;
     cameraControlsRef.current.removeAllEventListeners("controlstart");
+    viewportSizeRef.current = {
+      width: viewport.width,
+      height: viewport.height,
+    };
   });
-
-  const uiConfig = useUiStore();
-
-  useFrame(() => {});
 
   useEffect(() => {
     if (!cameraControlsRef.current) return;
     cameraControlsRef.current.zoomTo(uiConfig.targetZoomFactor, true);
-  }, [uiConfig]);
+    moveCamera({});
+  }, [uiConfig.targetZoomFactor]);
+
+  useEffect(() => {
+    updateCamera(pos);
+  }, [pos]);
+
+  useKeyPressEvent("ArrowUp", () => moveCamera({ y: -MOVE_DELTA }));
+  useKeyPressEvent("ArrowDown", () => moveCamera({ y: MOVE_DELTA }));
+  useKeyPressEvent("ArrowRight", () => moveCamera({ x: MOVE_DELTA }));
+  useKeyPressEvent("ArrowLeft", () => moveCamera({ x: -MOVE_DELTA }));
 
   return (
     <>
@@ -65,11 +117,13 @@ const Scene = () => {
         minZoom={MIN_ZOOM}
         maxZoom={MAX_ZOOM}
         dollyToCursor={false}
+        makeDefault
       />
+
       <Suspense
         fallback={<meshStandardMaterial attach="material" color="red" />}
       >
-        <mesh scale={[calcWfromH(viewport.height), viewport.height, 1]}>
+        <mesh scale={[planeSize[0], planeSize[1], 1]} ref={meshRef}>
           <planeGeometry />
           {mode === "HISTORY" ? (
             <VideoMaterial
@@ -84,6 +138,17 @@ const Scene = () => {
           )}
         </mesh>
       </Suspense>
+      {/* <CrossGrid
+        dX={CROSS_DELTA}
+        dY={CROSS_DELTA}
+        numW={stageSize[0] / CROSS_DELTA}
+        numH={stageSize[1] / CROSS_DELTA}
+        crossScale={0.01}
+      /> */}
+      <gridHelper
+        args={[10000, 100, 0xffffff, 0xffffff]}
+        rotation={[MathUtils.DEG2RAD * 90, 0, 0]}
+      />
     </>
   );
 };
--- src/routes/map/index.tsx
+++ src/routes/map/index.tsx
@@ -1,30 +1,12 @@
-import { useSpring, animated, SpringRef } from "@react-spring/web";
+import { ReactNode, useCallback, useState } from "react";
 
-import {
-  ReactNode,
-  useEffect,
-  useImperativeHandle,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-import { MAP_SIZE_X, MAP_SIZE_Y } from "../../CONSTANTS";
-
-import { history } from "../../data/CONSTANTS";
-import { remap } from "../../func/data";
-import { MapControls } from "@react-three/drei";
-
-import { Canvas, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 
 import ProjectInspector from "../../components/ProjectInspector";
-import { projects, positions, scenarios } from "../../data";
-import { useEffectOnce, useKeyPressEvent } from "react-use";
+import { projects } from "../../data";
 import Controller from "../../components/Contoller";
-import useMainStore from "../../stores/main";
-import { attachImage, createFutureRefs, createHistoryRef } from "./func";
 import useUiStore from "../../stores/ui";
 import Scene from "./Scene";
-import useKeyboardControls from "../../hooks/useKeyboardControls";
 
 import deviceFrame from "../../assets/device-frame.svg";
 
@@ -33,30 +15,51 @@ const Map = () => {
 
   const uiConfig = useUiStore();
 
-  useKeyboardControls();
+  /* const handleControlChange = (e: any) => {
+    const maxX = 90;
+    const minX = -90;
+    const maxY = 90;
+    const minY = -90;
 
-  const renderScene = (s: ReactNode) => {
-    console.log(uiConfig.showFrame);
+    if (!e.target) return;
+    const x = e.target.x;
+    const y = e.target.y;
 
-    if (uiConfig.showFrame) {
-      return (
-        <div className="relative overflow-hidden h-full">
-          <div className="w-full h-full z-0 absolute left-0 right-0 top-0 bottom-0">
-            {s}
-          </div>
-          <img src={deviceFrame} className="w-full h-full z-10 relative" />
-        </div>
-      );
-    } else {
-      return (
-        <div className="relative overflow-hidden h-full aspect-video">
-          <div className="w-full h-full z-0 absolute left-0 right-0 top-0 bottom-0">
-            {s}
-          </div>
-        </div>
-      );
+    if (x < minX || x > maxX) {
+      e?.target.target.setX(x < minX ? minX : maxX);
+      camera.position.setX(cameraLastPosition.current.x);
     }
-  };
+    if (y < minY || y > maxY) {
+      e?.target.target.setY(y < minY ? minY : maxY);
+      camera.position.setY(cameraLastPosition.current.y);
+    }
+    cameraLastPosition.current.x = camera.position.x;
+    cameraLastPosition.current.y = camera.position.y;
+  }; */
+
+  const renderScene = useCallback(
+    (s: ReactNode) => {
+      if (uiConfig.showFrame) {
+        return (
+          <div className="relative overflow-hidden h-full bg-green-300">
+            <div className="w-full h-full z-0 absolute left-0 right-0 top-0 bottom-0 bg-red-300">
+              {s}
+            </div>
+            <img src={deviceFrame} className="w-full h-full z-10 relative" />
+          </div>
+        );
+      } else {
+        return (
+          <div className="relative overflow-hidden h-full w-full">
+            <div className="w-full h-full z-0 absolute left-0 right-0 top-0 bottom-0">
+              {s}
+            </div>
+          </div>
+        );
+      }
+    },
+    [uiConfig]
+  );
 
   /* https://blog.promaton.com/camera-animations-with-r3f-and-react-spring-6fd378296c46 */
 
@@ -64,8 +67,8 @@ const Map = () => {
     <>
       <div className="w-full h-full flex flex-col justify-center items-center p-8 relative">
         {renderScene(
-          <Canvas resize={{ scroll: false }}>
-            <MapControls enableZoom={false} />
+          <Canvas resize={{}} orthographic className="w-full">
+            <color attach="background" args={["red"]} />
             <Scene />
           </Canvas>
         )}
