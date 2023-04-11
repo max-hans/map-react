65a60dda8378db4b3dc9cab00bef562708fdcc73
max-hans <m.hans@me.com>
2023-04-11 23:09:59 +0200

    update video

--- src/data/CONSTANTS.ts
+++ src/data/CONSTANTS.ts
@@ -1,7 +1,7 @@
 import { History } from "../types/data";
 
 export const history: History = {
-  src: "20230409_211451_output.mp4",
+  src: "output_264.mp4",
   from: new Date().setFullYear(1950),
   to: new Date().setFullYear(2020),
 };
