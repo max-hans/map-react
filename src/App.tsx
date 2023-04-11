67e2b5dc0e6fc5ed6180578b29d3f09ab1b7e7ff
max-hans <m.hans@me.com>
2023-04-11 22:36:45 +0200

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 export default function Root() {
   return (
-    <>
-      <a href="/map">map</a>
-    </>
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="bg-white mx-auto my-auto p-8 flex flex-col space-y-8">
+        <h2 className="underline">links</h2>
+        <a href="/dev">developer view</a>
+        <a href="/map">preview view</a>
+      </div>
+    </div>
   );
 }
