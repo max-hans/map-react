e3860917f708a309e114916cd1ac6ff623a194da
max-hans <m.hans@me.com>
2023-05-01 20:31:54 +0200

    update links

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ export default function Root() {
     <div className="w-full h-full flex flex-col justify-center items-center">
       <div className="bg-white mx-auto my-auto p-8 flex flex-col space-y-8">
         <h2 className="underline">links</h2>
-        <a href="/dev">developer view</a>
-        <a href="/map">preview view</a>
+        <a href="/mockup">mockup</a>
+        <a href="/full">full</a>
         <a href="/data/project">project</a>
         <a href="/data/title">title</a>
       </div>
