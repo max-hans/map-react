dde5b2f336b4c28b4291a22a338b10bb0954d63d
max-hans <m.hans@me.com>
2023-05-01 20:32:04 +0200

    full mode

--- src/routes/map/full.tsx
+++ src/routes/map/full.tsx
@@ -13,23 +13,19 @@ const FullMapView = () => {
   /* https://blog.promaton.com/camera-animations-with-r3f-and-react-spring-6fd378296c46 */
 
   return (
-    <>
-      <Frame>
-        <div className="relative overflow-hidden h-full w-full">
-          <div className="w-full h-full z-0 absolute left-0 right-0 top-0 bottom-0 bg-blue-300">
-            <Canvas
-              flat
-              orthographic
-              className="w-full"
-              camera={{ fov: 75, near: 0.01, far: 200, position: [0, 0, 100] }}
-            >
-              <color attach="background" args={["black"]} />
-              <Scene />
-            </Canvas>
-          </div>
-        </div>
-      </Frame>
-    </>
+    <div className="relative overflow-hidden h-full w-full bg-red-300">
+      <div className="w-full h-full z-0 absolute left-0 right-0 top-0 bottom-0 bg-blue-300">
+        <Canvas
+          flat
+          orthographic
+          className="w-full"
+          camera={{ fov: 75, near: 0.01, far: 200, position: [0, 0, 100] }}
+        >
+          <color attach="background" args={["black"]} />
+          <Scene />
+        </Canvas>
+      </div>
+    </div>
   );
 };
 export default FullMapView;
