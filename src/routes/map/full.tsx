301fcc21f6fbe09e1200222b7fbcabe0fb494a95
max-hans <m.hans@me.com>
2023-05-01 20:32:15 +0200

    style

--- src/routes/map/full.tsx
+++ src/routes/map/full.tsx
@@ -13,7 +13,7 @@ const FullMapView = () => {
   /* https://blog.promaton.com/camera-animations-with-r3f-and-react-spring-6fd378296c46 */
 
   return (
-    <div className="relative overflow-hidden h-full w-full bg-red-300">
+    <div className="relative overflow-hidden h-full w-full">
       <div className="w-full h-full z-0 absolute left-0 right-0 top-0 bottom-0 bg-blue-300">
         <Canvas
           flat
