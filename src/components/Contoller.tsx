f042e25c1024ab7abfe717804ce6c504c70827fd
max-hans <m.hans@me.com>
2023-05-03 20:42:14 +0200

    fmt

--- src/components/Contoller.tsx
+++ src/components/Contoller.tsx
@@ -57,128 +57,123 @@ const Controller = () => {
     onSelect(idx);
   };
 
-  return (
-    devMode && (
-      <div
-        className={`fixed top-8 right-8 bg-white p-4 flex flex-col space-y-4 rounded-md shadow-md w-72 z-50 transition-all ${
-          collapse ? "translate-x-full" : ""
-        }`}
-        style={{ transform: `${collapse ? "translate(100%,0)" : ""}` }}
-      >
+  return devMode ? (
+    <div
+      className={`fixed top-8 right-8 bg-white p-4 flex flex-col space-y-4 rounded-md shadow-md w-72 z-50 transition-all ${
+        collapse ? "translate-x-full" : ""
+      }`}
+      style={{ transform: `${collapse ? "translate(100%,0)" : ""}` }}
+    >
+      <button onClick={() => setCollapse((state) => !state)} className="w-min">
+        hide
+      </button>
+      <h2>data</h2>
+      <Slider
+        title="time"
+        value={time}
+        onValue={setTime}
+        min={1950}
+        max={2020}
+      />
+      <div className="grid grid-cols-2 gap-2">
         <button
-          onClick={() => setCollapse((state) => !state)}
-          className="w-min"
+          onClick={() => handleScrub(-1)}
+          className="py-2 text-center w-full border border-gray-300 rounded-md"
         >
-          hide
+          {"<<"}
         </button>
-        <h2>data</h2>
-        <Slider
-          title="time"
-          value={time}
-          onValue={setTime}
-          min={1950}
-          max={2020}
-        />
-        <div className="grid grid-cols-2 gap-2">
-          <button
-            onClick={() => handleScrub(-1)}
-            className="py-2 text-center w-full border border-gray-300 rounded-md"
-          >
-            {"<<"}
-          </button>
-          <button
-            onClick={() => handleScrub(1)}
-            className="py-2 text-center w-full border border-gray-300 rounded-md"
-          >
-            {">>"}
-          </button>
-        </div>
-        <Slider
-          title="scenario"
-          value={scenario}
-          onValue={setScenario}
-          min={0}
-          max={scenarios.length - 1}
-          step={1}
-        />
-        <div className="w-full">
-          <div className="flex flex-row justify-between">
-            <h3 className="font-bold">mode</h3>
-            <p className="italic">{mode}</p>
-            <button
-              onClick={() => setMode("HISTORY")}
-              className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
-            >
-              history
-            </button>
-            <button
-              onClick={() => setMode("FUTURE")}
-              className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
-            >
-              future
-            </button>
-          </div>
-        </div>
         <button
-          onClick={() => onSelect(null)}
-          className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
+          onClick={() => handleScrub(1)}
+          className="py-2 text-center w-full border border-gray-300 rounded-md"
         >
-          deselect
+          {">>"}
         </button>
-        <h2>navigation</h2>
-        <Slider
-          title="zoom"
-          value={uiConfig.targetZoomFactor}
-          onValue={uiConfig.setTargetZoomFactor}
-          min={MIN_ZOOM}
-          max={MAX_ZOOM}
-          useFloat
-          step={0.1}
-        />
-        <div className="grid grid-cols-2 gap-2">
+      </div>
+      <Slider
+        title="scenario"
+        value={scenario}
+        onValue={setScenario}
+        min={0}
+        max={scenarios.length - 1}
+        step={1}
+      />
+      <div className="w-full">
+        <div className="flex flex-row justify-between">
+          <h3 className="font-bold">mode</h3>
+          <p className="italic">{mode}</p>
           <button
-            onClick={() => handleMove("up")}
+            onClick={() => setMode("HISTORY")}
             className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
           >
-            up
+            history
           </button>
           <button
-            onClick={() => handleMove("left")}
-            className="py-2 text-center w-full border border-gray-300 rounded-md"
-          >
-            left
-          </button>
-          <button
-            onClick={() => handleMove("right")}
-            className="py-2 text-center w-full border border-gray-300 rounded-md"
-          >
-            right
-          </button>
-          <button
-            onClick={() => handleMove("down")}
+            onClick={() => setMode("FUTURE")}
             className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
           >
-            down
+            future
           </button>
-        </div>
-        <button
-          onClick={selectRandomProject}
-          className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
-        >
-          select random project
-        </button>
-        <h2>view</h2>
-        <div className="w-full flex flex-row space-x-4">
-          <div className="underline text-blue-600">
-            <a href="/map">/standard</a>
-          </div>
-          <div className="underline text-blue-600">
-            <a href="/dev">/develop</a>
-          </div>
         </div>
       </div>
-    )
-  );
+      <button
+        onClick={() => onSelect(null)}
+        className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
+      >
+        deselect
+      </button>
+      <h2>navigation</h2>
+      <Slider
+        title="zoom"
+        value={uiConfig.targetZoomFactor}
+        onValue={uiConfig.setTargetZoomFactor}
+        min={MIN_ZOOM}
+        max={MAX_ZOOM}
+        useFloat
+        step={0.1}
+      />
+      <div className="grid grid-cols-2 gap-2">
+        <button
+          onClick={() => handleMove("up")}
+          className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
+        >
+          up
+        </button>
+        <button
+          onClick={() => handleMove("left")}
+          className="py-2 text-center w-full border border-gray-300 rounded-md"
+        >
+          left
+        </button>
+        <button
+          onClick={() => handleMove("right")}
+          className="py-2 text-center w-full border border-gray-300 rounded-md"
+        >
+          right
+        </button>
+        <button
+          onClick={() => handleMove("down")}
+          className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
+        >
+          down
+        </button>
+      </div>
+      <button
+        onClick={selectRandomProject}
+        className="py-2 text-center w-full col-span-2 border border-gray-300 rounded-md"
+      >
+        select random project
+      </button>
+      <h2>view</h2>
+      <div className="w-full flex flex-row space-x-4">
+        <div className="underline text-blue-600">
+          <a href="/map">/standard</a>
+        </div>
+        <div className="underline text-blue-600">
+          <a href="/dev">/develop</a>
+        </div>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default Controller;
