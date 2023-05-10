3e277a0fd774dfcca16baea61eef7d46943a6aa1
max <m.hans@me.com>
2023-05-10 20:13:49 +0200

    update consts

--- src/CONSTANTS.ts
+++ src/CONSTANTS.ts
@@ -6,15 +6,15 @@ export const MAP_SIZE_Y = 1080;
 export const MIN_ZOOM = 1;
 export const MAX_ZOOM = 5;
 
-export const ZOOM_THRESHOLD = 5;
-export const ZOOM_SCALE_FACTOR = 0.1;
+export const ZOOM_THRESHOLD = 10;
+export const ZOOM_SCALE_FACTOR = 0.005;
 export const CAMERA_UPDATE_INTERVAL = 50;
 
 export const YEARS_MIN = 1950;
 export const YEARS_MAX = 2023;
 
-export const MOVEMENT_THRESHOLD = 10;
-export const MOVEMENT_SCALE_FACTOR = 0.1;
+export const MOVEMENT_THRESHOLD = 5;
+export const MOVEMENT_SCALE_FACTOR = 0.15;
 
 export const MESSAGE_TIMEOUT = 3000;
 
