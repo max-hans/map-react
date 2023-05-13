005efc9222b5c908210f497863e2d93f7ec5ba6b
max-hans <m.hans@me.com>
2023-05-13 16:51:07 +0200

    adjust path

--- src/routes/map/res/futures/index.ts
+++ src/routes/map/res/futures/index.ts
@@ -1,7 +1,7 @@
 import future0 from "./simulation-ssp1-1.9.jpg";
 import future1 from "./simulation-ssp1-2.6.jpg";
 import future2 from "./simulation-ssp2-4.5.jpg";
-import future3 from "./simulation-ssp3-7.i.jpg";
+import future3 from "./simulation-ssp3-7.0.jpg";
 import future4 from "./simulation-ssp5-8.5.jpg";
 
 export default [future0, future1, future2, future3, future4];
